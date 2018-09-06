1e16f3953e40753cb20106436a4afee8518c1c15
nmedan <nmedan92@gmail.com>
2018-09-06 18:50:43 +0200

    Zadatak 1

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 
 // Onsen UI Styling and Icons
-require('onsenui/css-components-src/src/onsen-css-components.css');
+/*require('onsenui/css-components-src/src/onsen-css-components.css');
+require('onsenui/css/onsenui.css');*/
+
 require('onsenui/css/onsenui.css');
+require('../assets/onsen-css-components.css')
+require('../assets/theme.css')
 
 import App from './App.vue';
 
