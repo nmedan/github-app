e4ca26d2094aeeeb866119b1d5e86c8645ad36e7
nmedan <nmedan92@gmail.com>
2018-09-06 22:02:00 +0200

    Zadatak 6

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
    <v-ons-page>
       <AppToolbar>
-        <template slot="left"></template>  
-                     
+        <template slot="left"></template>                       
         <template slot="right"></template>
       </AppToolbar>    
       <AppSearch class="center" :query.sync="query" />   
+      <div v-if="repositories.length">
+        <v-ons-list >
+          <v-ons-list-header>
+              Repositories of {{ query }}
+          </v-ons-list-header>
+          <v-ons-list-item v-for="(repository, key) in repositories" :key="key">
+              <div class="left">
+                 <img class="list-item__thumbnail" :src="repository.owner.avatar_url"></img>
+              </div>
+              <div class="center">
+                 <span class="list_item__title"> Name: {{repository.name}} </span>
+                 <span class="list_item__subtitle"> Description: {{repository.description}} </span>
+              </div>
+          </v-ons-list-item>
+        </v-ons-list>
+      </div>
+      <div v-else>
+        No repositories
+      </div>
    </v-ons-page>
 </template>
-<script>
 
+<script>
 import AppToolbar from './components/AppToolbar.vue'
 import AppSearch from './components/AppSearch.vue'
 import debounce from 'lodash/debounce'
