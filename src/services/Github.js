626f7f835d20252ce57c0d228c691c604d4beacd
nmedan <nmedan92@gmail.com>
2018-09-06 20:43:57 +0200

    Zadatak 4

---
+++ src/services/Github.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export default class Github {
+    constructor () {
+      axios.defaults.baseURL = `https://api.github.com`
+    }
+  
+    getRepos(username) {
+      return axios.get(`/users/${username}/repos`)
+    }
+
+  }
+  
+  export const github = new Github()
